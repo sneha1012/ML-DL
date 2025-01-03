d79443f83e062a0364982f2c2e22cf46279a6d7f
Sneha Maurya <snehamaurya1012@gmail.com>
2025-01-03 13:22:51 -0500

    numpy indexing

--- Numpy-Vectorisation.py
+++ Numpy-Vectorisation.py
@@ -6,4 +6,23 @@ import time
 #Slicing - getting  a subset of elements from an array based on their indices.
 ##vector Creation
 
+a = np.zeros(4);      print(f"np.zeros(4): a = {a}, a data type = {a.dtype}")
+a = np.zeros((4, ));  print(f"np.zeros(4,)  a ={a}, a shape = {a.shape}")
+a = np.random.random_sample(4); print(f"np.random.random_sample(4): a = {a}, a shape = {a.shape}")
+
+#We can use np.arrange, which creates a evenlenly spaces array using numbers within a specific range.
+
+#Indexing and slicing
+
+a = np.arange(10)
+print(a)
+
+#accesing an element 
+print(f"a[2].shape: {a[2].shape} a[2], accesing an element returns a scalar")
+
+#negative index, count from the end
+print((f"a[-1] = {a[-1]}"))
+
+#accesing should should be done of indexes within the range otherwise they will rpoduce and error.
+
 
